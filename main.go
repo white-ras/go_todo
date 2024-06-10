e625992bb122362a3f30c0323142a024cb9801f1
white-ras <rasras3366@gmail.com>
2024-06-10 15:37:51 +0900

    feat: ログイン機能を実装

--- main.go
+++ main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 )
 
 func main() {
-		// fmt.Println(models.Db)
+		fmt.Println(models.Db)
 
-		// controllers.StartMainServer()
-		user, _ := models.GetUserByEmail("test@test.com")
-		fmt.Println(user)
-
-		session, err := user.CreateSession()
-		if err != nil {
-			log.Println(err)
-		}
-		fmt.Println(session)
-
-		valid, _ := session.CheckSession()
-		fmt.Println(valid)
+		controllers.StartMainServer()
 }
